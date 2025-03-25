aa9b7ced3c2e1ca8a13d1b2e2cc5ec40ace5ef2b
renanrlopes <rrlopes7@gmail.com>
2025-03-24 23:19:39 -0300

    Sum of the two nubers

---
+++ day2/sum_two_numbers2.py
@@ -0,0 +1,7 @@
+def sum(n1, n2):
+    return n1 + n2
+
+num1 = input("First number: ")
+num2 = input("Second number: ")
+
+print(sum(int(num1), int(num2)))
---
+++ day2/sum_two_nunbers.py
@@ -0,0 +1,6 @@
+number1 = input("First number: ")
+number2 = input("Second number: ")
+
+sum = int(number1) + float(number2)
+
+print("the sum of the two numbers is:", sum)
